8bb352655ffabbf65fd6c65d91a3e61487147ac0
Vignesh S <vignesh6993@live.in>
2020-01-24 09:42:46 +0000

    Added Matrix class which does all calulations in an immutable way

--- main.dart
+++ main.dart
@@ -1,3 +1,64 @@
+import 'dart:math';
+import './matrix.dart';
+
+Random rnd = new Random();
+
+double map(double x, double in_min, double in_max, double out_min, double out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+class NeuralNetwork {
+  int inputNodes;
+  int hiddenNodes;
+  int outputNodes;
+
+  NeuralNetwork(int inputNodes, int hiddenNodes, int outputNodes) {
+    this.inputNodes = inputNodes;
+    this.hiddenNodes = hiddenNodes;
+    this.outputNodes = outputNodes;
+  }
+}
+
+class Perceptron {
+  final weights = new List<double>(2);
+
+  Perceptron() {
+    for(int i = 0; i < weights.length; i++) {
+      weights[i] = map(rnd.nextDouble(), 0, 1, -1, 1);
+    }
+    print(weights);
+  }
+
+  double weightedSum(List<double> inputs) {
+    double sum = 0;
+    for(int i = 0; i < inputs.length; i++) {
+      sum += inputs[i]*weights[i];
+    }
+    return sum;
+  }
+}
+
 void main() {
-	print('Hello World!');
+  //var p = new Perceptron();
+  //print(p.weightedSum([0.8, 0.2]));
+  // NeuralNetwork brain = NeuralNetwork(3,4,1);
+  Matrix m1 = Matrix(3,4);
+  Matrix m2 = Matrix(4,3);
+  m1 = m1.randomize();
+  m2 = m2.randomize();
+  // print(m1);
+  // print(m1.multiply(2));
+  // print(m1);
+  // print(m1.transpose());
+  // print(m1);
+  print(m1);
+  print(m2);
+  print(m1.multiply(m2));
+  print(m1);
+  print(m2);
+
+  int x = 5;
+  print(x);
+  x = 10;
+  print(x);
 }
---
+++ matrix.dart
@@ -0,0 +1,140 @@
+import 'dart:math';
+
+// Immutable Matrix Class
+class Matrix {
+  int rows;
+  int cols;
+  List<List<double>> matrix;
+  Random rnd = new Random();
+
+  Matrix(int rows, int cols, {int rngSeed = null}) {
+    this.rows = rows;
+    this.cols = cols;
+    if(rngSeed != null) {
+      rnd = new Random(rngSeed);
+    }
+    this.matrix = new List.generate(this.rows, (_) => new List(this.cols));
+
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        matrix[i][j] = 0.0;
+      }
+    }
+  }
+
+  Matrix transpose() {
+    Matrix result = new Matrix(cols, rows);
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        result.matrix[j][i] = matrix[i][j];
+      }
+    }
+    // matrix = new List.generate(cols, (_) => new List(rows));
+    // int tmpCols = cols;
+    // cols = rows;
+    // rows = tmpCols;
+    // matrix = temp.matrix;
+    return result;
+  }
+
+  Matrix multiply(var val, {bool hadamard = false}) {
+    if(val is Matrix && !hadamard) {
+      // Dot product
+      if(cols != val.rows) {
+        throw ("The colums of A must match rows of B $cols, ${val.rows}");
+      }
+      Matrix result = new Matrix(rows, val.cols);
+      var a = matrix;
+      var b = val.matrix;
+      for(int i = 0; i < result.rows; i++) {
+        for(int j = 0; j < result.cols; j++) {
+          var sum = 0.0;
+          for(int k = 0; k < cols; k++) {
+            sum += a[i][k] * b[k][j];
+          }
+          result.matrix[i][j] = sum;
+        }
+      }
+      return result;
+    }
+
+    Matrix result = clone();
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        if(val is Matrix && hadamard) {
+          // Hadamard product
+          result.matrix[i][j] *= val.matrix[i][j];
+        } else {
+          // Scalar product
+          result.matrix[i][j] *= val;
+        }
+      }
+    }
+    return result;
+  }
+
+  Matrix clone() {
+    Matrix result = new Matrix(rows, cols);
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        result.matrix[i][j] = matrix[i][j];
+      }
+    }
+    return result;
+  }
+
+  Matrix add(var val) {
+    Matrix result = clone();
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        if(val is Matrix) {
+          result.matrix[i][j] += val.matrix[i][j];
+        } else {
+          result.matrix[i][j] += val;
+        }
+      }
+    }
+    return result;
+  }
+
+  Matrix ones() {
+    Matrix result = clone();
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        result.matrix[i][j] = 1.0;
+      }
+    }
+    return result;
+  }
+
+  Matrix zeros() {
+    Matrix result = clone();
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        result.matrix[i][j] = 0.0;
+      }
+    }
+    return result;
+  }
+
+  Matrix randomize() {
+    Matrix result = clone();
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        result.matrix[i][j] = rnd.nextDouble();
+      }
+    }
+    return result;
+  }
+
+  String toString() {
+    String m = "";
+    for(int i = 0; i < rows; i++) {
+      for(int j = 0; j < cols; j++) {
+        m += matrix[i][j].toStringAsFixed(3) + ((j + 1 == cols) ? "" : ", ");
+      }
+      m += '\n';
+    }
+    return m;
+  }
+}
